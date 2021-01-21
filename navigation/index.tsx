33ccd72310b16ef76f6de789c6e7379952489160
LacTran <trangialac.fin@gmail.com>
2021-01-21 02:09:06 +0200

    implement navigation

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,12 +1,15 @@
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
+import { ColorSchemeName, StyleSheet, View } from 'react-native';
 
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
+import MainTabNavigator from './MainTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
+
+import color from '../constants/Colors';
+import { Octicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
@@ -26,9 +29,42 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
+    <Stack.Navigator screenOptions={{
+      headerStyle: styles.headerStyle,
+      headerTintColor: color.light.background,
+      headerTitleAlign: 'left',
+      headerTitleStyle: {
+        fontWeight: 'bold'
+      }
+    }}>
+      <Stack.Screen
+        name="Root"
+        component={MainTabNavigator}
+        options={{
+          title: "WhatsApp",
+          headerRight: () => (
+            <View style={styles.headerIconsStyle}>
+              <Octicons name="search" size={22} color={color.light.background} />
+              <MaterialCommunityIcons name="dots-vertical" size={22} color={color.light.background} />
+            </View>
+          )
+        }}
+      />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: color.light.tint,
+    shadowOpacity: 0, //ios
+    elevation: 0 //android
+  },
+  headerIconsStyle: {
+    flexDirection: 'row',
+    width: 60,
+    justifyContent: 'space-between',
+    marginRight: 10
+  }
+})
