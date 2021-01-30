a307aad4914d275b8f0fe922d9d12783d82ce89b
LacTran <trangialac.fin@gmail.com>
2021-01-30 02:00:28 +0200

    init aws-amplify

--- App.tsx
+++ App.tsx
@@ -2,6 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+// configure aws-amplify
+import Amplify from 'aws-amplify'
+import config from './aws-exports';
+Amplify.configure(config);
+
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
