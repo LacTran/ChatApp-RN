d29ea3c6b78d1dcf913120ab5241bebe7257d55a
LacTran <trangialac.fin@gmail.com>
2021-02-05 10:52:35 +0200

    display users fetched from API

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,23 +1,48 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import { ContactListItem } from '../components/ContactListItem/ContactListItem';
 import { NewMessageButton } from '../components/NewMessageButton/NewMessageButton';
 
-import ContactData from '../data/Users';
+// dummy data
+// import ContactData from '../data/Users';
+
+import { API, graphqlOperation } from 'aws-amplify';
+import { listUsers } from '../src/graphql/queries';
 
 export default function ContactsScreen() {
+
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const contactsData = await API.graphql(
+          graphqlOperation(
+            listUsers
+          )
+        )
+
+        setContacts(contactsData.data.listUsers.items)
+
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    fetchContacts();
+
+  }, [])
+
   return (
     <View>
       <FlatList
-        data={ContactData}
+        data={contacts}
         renderItem={({ item }) =>
           <ContactListItem user={item} />
         }
         keyExtractor={(item) => item.id}
         style={{ width: '100%' }}
       />
-
-      <NewMessageButton />
     </View>
   );
 }
